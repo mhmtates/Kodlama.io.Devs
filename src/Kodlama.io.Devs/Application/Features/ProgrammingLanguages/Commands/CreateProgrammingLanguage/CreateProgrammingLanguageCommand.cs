51985a68491e4e688af44bd1d54b3edadd5d0220
Mehmet <ozatesatess@gmail.com>
2022-09-03 05:26:36 +0300

    Create Command

---
+++ src/Kodlama.io.Devs/Application/Features/ProgrammingLanguages/Commands/CreateProgrammingLanguage/CreateProgrammingLanguageCommand.cs
@@ -0,0 +1,44 @@
+﻿using Application.Features.ProgrammingLanguages.Dtos;
+using Application.Services.Repositories;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.ProgrammingLanguages.Commands.CreateProgrammingLanguage
+{
+    public class CreateProgrammingLanguageCommand:IRequest<CreatedProgrammingLanguageDto>
+    {
+
+        public string Name { get; set; }
+
+
+        public class CreateProgrammingLanguageCommandHandler : IRequestHandler<CreateProgrammingLanguageCommand, CreatedProgrammingLanguageDto>
+        {
+            private readonly IProgrammingLanguageRepository _programmingLanguageRepository;
+            private readonly IMapper _mapper;
+
+            public CreateProgrammingLanguageCommandHandler(IProgrammingLanguageRepository programmingLanguageRepository, IMapper mapper)
+            {
+                _programmingLanguageRepository = programmingLanguageRepository;
+                _mapper = mapper;
+            }
+
+            public async Task<CreatedProgrammingLanguageDto> Handle(CreateProgrammingLanguageCommand request, CancellationToken cancellationToken)
+            {
+               ProgrammingLanguage mappedprogrammingLanguage = _mapper.Map<ProgrammingLanguage>(request);
+               ProgrammingLanguage createdprogrammingLanguage = await _programmingLanguageRepository.AddAsync(mappedprogrammingLanguage);
+               CreatedProgrammingLanguageDto createdprogrammingLanguageDto = _mapper.Map<CreatedProgrammingLanguageDto>(createdprogrammingLanguage);
+               return createdprogrammingLanguageDto;
+            }
+        }
+
+
+
+
+    }
+}
